3d9f59df0e3f1ac5e9d4bbf1025645edf6253771
Nick Ellinas <ngreek54@gmail.com>
2017-12-17 12:01:32 +0200

    Make keras example working with dummy data

--- example.py
+++ example.py
@@ -1,8 +1,12 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import InputLayer, Dense, BatchNormalization, Activation, Dropout
+
 from sacred import Experiment
 from sacred.observers import MongoObserver
 
 ex = Experiment('My_Experiment')
-my_url = 127.0.0.1:27017  # Or <server-static-ip>:<port> if running on server
+my_url = '127.0.0.1:27017'  # Or <server-static-ip>:<port> if running on server
 ex.observers.append(MongoObserver.create(url=my_url,
                                          db_name='my_database'))
 
@@ -12,12 +16,30 @@ def dnn_config():
     input_dim = 100
     output_dim = 20
     neurons = 300
+    activation = 'relu'
     dropout = 0.4
 
 @ex.automain
-def dnn_main(input_dim, output_dim, neurons, dropout, _run):  # Include _run in input for tracking metrics
-    x_train, y_train, x_valid, y_valid = load_data()  # Custom function in some other place
-    model = dnn_load_model(input_dim, output_dim, neurons, dropout)  # Custom function in some other place
+def dnn_main(input_dim, output_dim, neurons, activation, dropout, _run):  # Include _run in input for tracking metrics
+    # Dummy data
+    x_train = np.random.randn(1000, 100)
+    y_train = np.random.randn(1000, 20)
+    x_valid = np.random.randn(1000, 100)
+    y_valid = np.random.randn(1000, 20)
+
+    # Model architecture
+    model = Sequential()
+    model.add(InputLayer(batch_input_shape=(None, input_dim), name='input'))
+    model.add(Dense(units=neurons, name='hidden'))
+    model.add(BatchNormalization())
+    model.add(Activation(activation=activation))
+    model.add(Dropout(rate=dropout))
+
+    # Compile model
+    model.compile(optimizer='Adam',
+                  loss='mse')
+
+    # Training and validation
     history = model.fit(x=x_train, y=y_train,
                         verbose=2,
                         validation_data=(x_valid, y_valid))
